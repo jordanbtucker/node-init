272ddc7cbff665831b65a349bce78249cbfb7e8d
Jordan Tucker <jordanbtucker@gmail.com>
2020-09-10 09:07:40 -0500

    test: use config in tests

--- test/main.js
+++ test/main.js
@@ -1,12 +1,12 @@
-require('dotenv/config')
 const sinon = require('sinon')
 const t = require('tap')
 
+const {name} = require('../lib/config')
 const main = require('../lib/main')
 
 t.test('main', async t => {
   await t.test(`logs 'Hello, $NAME!'`, async () => {
-    const message = `Hello, ${process.env.NAME || 'user'}!`
+    const message = `Hello, ${name}!`
 
     const mock = sinon.mock(console)
     mock.expects('log').once().withExactArgs(message)
